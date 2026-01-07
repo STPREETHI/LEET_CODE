9f52e333f18bb37000c572ffaccd026b2ada876d
STPREETHI <158798657+STPREETHI@users.noreply.github.com>
2026-01-07 12:38:16 +0530

    Time: 2130 ms (6.36%) | Memory: 21 MB (5.84%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        result=[]
+        for i in range(n):
+            max_val=-1
+            for j in range(i+1,i+n):
+                idx=j%n
+                if nums[idx]>nums[i]:
+                    max_val=nums[idx]
+                    break
+            result.append(max_val)
+        return result
+
+        
