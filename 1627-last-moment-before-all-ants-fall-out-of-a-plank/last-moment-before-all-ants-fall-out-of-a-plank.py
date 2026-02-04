1812dbccb71f96e60d5cfe7ba3ae4cdc84a31455
STPREETHI <158798657+STPREETHI@users.noreply.github.com>
2026-02-04 14:31:49 +0530

    Time: 5 ms (27.70%) | Memory: 20.3 MB (13.61%) - LeetSync

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
+        res=0
+        for i in range(len(left)):
+            res=max(res,left[i])
+        for i in range(len(right)):
+            res=max(res,n-right[i])
+        return res
+
+        
